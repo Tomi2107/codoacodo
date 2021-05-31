0b0540ba45b54374759d5946682ee917d6b8e6ea
Tomi2107 <83030653+Tomi2107@users.noreply.github.com>
2021-05-31 14:28:01 -0300

    Add files via upload

---
+++ pomedio/public_html/promedio.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var nombre = prompt('Escriba su nombre');
+var nota1 = prompt('ingrese su primera nota');
+var nota2 = prompt('ingrese su segunda nota');
+var nota3 = prompt('ingrese su tercera nota');
+var promedio =((+nota1 + +nota2+ +nota3)/3);
+alert('Alumno: '+nombre+' promedio: '+ promedio);
+    
+
+
+
+
+
+
